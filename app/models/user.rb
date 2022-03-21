a59e885c3d79661563c494e79b56c0de2b2a4149
SettingTom <tnbt0277@gmail.com>
2022-03-21 15:11:00 +0900

    新規登録できるときのテストコードの記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters'} do
     validates :family_name
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-  
+  describe "ユーザー新規登録" do
+    context '新規登録できるとき' do
+      it 'nickname、email、passwordとpassword_confirmation、
+          family_name、family_name、family_name_kana、family_name_kana、birth_dayが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end  
+    end
+  end
+
 end
