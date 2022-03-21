753ddf04285ca8b529a86ff5dccb3326571c5ecf
SettingTom <tnbt0277@gmail.com>
2022-03-21 13:56:04 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters'} do
+    validates :family_name
+    validates :first_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: 'Input full-width katakana characters'} do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+  
   validates :birth_day, presence: true
 
   # has_many :prototypes
