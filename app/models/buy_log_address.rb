9de10a7e6cfe57bacd668e46872c76efd42bccf0
SettingTom <tnbt0277@gmail.com>
2022-04-15 14:37:16 +0900

    formの実装、購入画面の遷移の実装

--- app/models/buy_log_address.rb
+++ app/models/buy_log_address.rb
@@ -1,13 +1,13 @@
 class BuyLogAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :area_id, :municipalities, :block_number, :apartment, :phone_number, :buy_log, :user, :item, 
+  attr_accessor :post_code, :area_id, :municipalities, :block_number, :apartment, :phone_number, :buy_log, :user, :item
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipalities
     validates :block_number
-    validates :phone_number, format: {with: /\d{10,11}$/, message: "is invalid. Input only number"}
+    validates :phone_number, format: {with: /\d{10,11}\z/, message: "is invalid. Input only number"}
     validates :buy_log
     validates :user
     validates :item
