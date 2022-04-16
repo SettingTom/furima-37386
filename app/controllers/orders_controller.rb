184e164d3ee27f7518ef557f143e98a0b961b401
SettingTom <tnbt0277@gmail.com>
2022-04-16 13:31:42 +0900

    購入ページの画像、商品名、送料負担の反映

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,23 @@
 class OrdersController < ApplicationController
   def index
     @buy_log_address = BuyLogAddress.new
+    @item = Item.find(params[:item_id])
+    @delivery_pays = DeliveryPay.find(@item.delivery_pay_id)
   end
 
   def create
-    binding.pry
-    # @buy_log_address = BuyLogAddress.new(order_params)
-    # redirect_to "/items/#{order.item.id}"
+    @buy_log_address = BuyLogAddress.new(order_params)
+    if @buy_log_address.valid?
+      @buy_log_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
 
   def order_params
-    params.require(:buy_log_address).permit(:post_code, :area_id, :municipalities, :block_number, :apartment, :phone_number).merge(user_id: current_user.id)
+    params.require(:buy_log_address).permit(:post_code, :area_id, :municipalities, :block_number, :apartment, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/buy_log_address.rb
+++ app/models/buy_log_address.rb
@@ -1,6 +1,6 @@
 class BuyLogAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :area_id, :municipalities, :block_number, :apartment, :phone_number, :buy_log, :user, :item
+  attr_accessor :post_code, :area_id, :municipalities, :block_number, :apartment, :phone_number, :buy_log, :user_id, :item_id
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -9,12 +9,12 @@ class BuyLogAddress
     validates :block_number
     validates :phone_number, format: {with: /\d{10,11}\z/, message: "is invalid. Input only number"}
     validates :buy_log
-    validates :user
-    validates :item
+    validates :user_id
+    validates :item_id
   end
 
   def save
-    BuyLog.create(user: user, item:item)
+    BuyLog.create(user_id: user_id, item_id:item_id)
     Address.create(post_code: post_code, area_id: area_id, municipalities: municipalities, block_number: block_number, apartment: apartment, buy_log: buy_log)
   end
 end
