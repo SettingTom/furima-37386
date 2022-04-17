70bf8ebe4f12ae1616d0f74bd38ec4adccf306fd
SettingTom <tnbt0277@gmail.com>
2022-04-18 08:03:28 +0900

    PAY.JPの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :set_item
+
   def index
     @buy_log_address = BuyLogAddress.new
-    @item = Item.find(params[:item_id])
-    @delivery_pays = DeliveryPay.find(@item.delivery_pay_id)
   end
 
   def create
     @buy_log_address = BuyLogAddress.new(order_params)
     if @buy_log_address.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
       @buy_log_address.save
       redirect_to root_path
     else
@@ -16,8 +22,12 @@ class OrdersController < ApplicationController
   end
 
   private
+  def set_item
+    @item = Item.find(params[:item_id])
+    @delivery_pays = DeliveryPay.find(@item.delivery_pay_id)
+  end
 
   def order_params
-    params.require(:buy_log_address).permit(:post_code, :area_id, :municipalities, :block_number, :apartment, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:buy_log_address).permit(:post_code, :area_id, :municipalities, :block_number, :apartment, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
--- app/models/buy_log_address.rb
+++ app/models/buy_log_address.rb
@@ -1,6 +1,6 @@
 class BuyLogAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :area_id, :municipalities, :block_number, :apartment, :phone_number, :buy_log, :user_id, :item_id
+  attr_accessor :post_code, :area_id, :municipalities, :block_number, :apartment, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -8,13 +8,13 @@ class BuyLogAddress
     validates :municipalities
     validates :block_number
     validates :phone_number, format: {with: /\d{10,11}\z/, message: "is invalid. Input only number"}
-    validates :buy_log
+    validates :token, presence: true
     validates :user_id
     validates :item_id
   end
 
   def save
-    BuyLog.create(user_id: user_id, item_id:item_id)
-    Address.create(post_code: post_code, area_id: area_id, municipalities: municipalities, block_number: block_number, apartment: apartment, buy_log: buy_log)
+    buy_log = BuyLog.create(user_id: user_id, item_id:item_id)
+    Address.create(post_code: post_code, area_id: area_id, municipalities: municipalities, block_number: block_number, apartment: apartment, phone_number: phone_number, buy_log_id: buy_log.id)
   end
 end
