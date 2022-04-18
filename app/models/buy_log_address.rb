e5ab16e81fdd95a0f196250b1750239dcb23f5aa
SettingTom <tnbt0277@gmail.com>
2022-04-19 07:58:29 +0900

    テストコードの実装

--- app/models/buy_log_address.rb
+++ app/models/buy_log_address.rb
@@ -3,12 +3,13 @@ class BuyLogAddress
   attr_accessor :post_code, :area_id, :municipalities, :block_number, :apartment, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipalities
     validates :block_number
-    validates :phone_number, format: {with: /\d{10,11}\z/, message: "is invalid. Input only number"}
-    validates :token, presence: true
+    validates :phone_number, numericality: {only_integer: true, message: "is invalid. Input only number"}
+    validates :phone_number, format: {with: /\d{10,11}\z/, message: "is too short"}
+    validates :token
     validates :user_id
     validates :item_id
   end
---
+++ spec/factories/buy_log_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :buy_log_address do
+    post_code { '123-4567' }
+    area_id { 2 }
+    municipalities { '東京都' }
+    block_number { '1-1' }
+    apartment { '東京ハイツ' }
+    phone_number { Faker::Number.between(from: 0000000000, to: 99999999999) }
+    token {"tok_abcdefghijk00000000000000000"}
+  end
+end
